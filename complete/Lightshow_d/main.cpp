62ee93bcd28915e9611b7f7582ba5740bde1f4f8
dimembermatt <dimembermatt@gmail.com>
2018-12-27 17:48:19 -0600

    added Lightshow, MineralPatterns projects

--- complete/Lightshow_d/main.cpp
+++ complete/Lightshow_d/main.cpp
@@ -1,6 +1,14 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+/**
+ *	@Author: Matthew Yu
+ *	@Title:	Lightshow (GA notebook 012)
+ *	@Last Modified: 12/27/18
+ *	@Bugs:	Mirrors with tilt !(90deg, 45deg) cause particles to lose velocity
+ *		over time (float data loss from arctan/cos/sin)
+ */
+
 //========================================================================
 int main( ){
 	ofSetupOpenGL(800,400,OF_WINDOW);			// <-------- setup the GL context
