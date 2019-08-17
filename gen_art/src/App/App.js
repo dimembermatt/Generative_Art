02858ba58f54981aeb8d33108ea33d3852fa8de8
Matthew Yu <matthewjkyu@gmail.com>
2019-08-16 20:01:52 -0400

    testing prod build of dev.

--- gen_art/src/App/App.js
+++ gen_art/src/App/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
 import SectionList from './SectionList';
-import Page from './Page';
 
 class App extends React.Component {
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route
-                        path="/pages/:id"
-                        component={Page}
-                    />
-                    <Route
-                        path="/"
-                        component={SectionList}
-                    />
-                </Switch>
-            </BrowserRouter>
+            <SectionList></SectionList>
         );
     }
 }
